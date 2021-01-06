583a23a4dcd122b8444b3515b5957817ac4ea860
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-06 18:07:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 planeteer_calls.find do |element|
-  element = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  valid_calls.include? (element)
 end
 end
